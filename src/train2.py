c0e893c113618e9d18684fbeab45518e5b9be8b8
Anuj Shah <20814689+anujshah1003@users.noreply.github.com>
2026-07-01 14:46:21 +0530

    config driven pipeline

--- src/train2.py
+++ src/train2.py
@@ -1,13 +1,16 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+import yaml
+
+params = yaml.safe_load(open("params.yaml"))
 
 df = pd.read_csv("data/processed.csv")
 
 X = df.drop("target", axis=1)
 y = df["target"]
 
-model = RandomForestClassifier(n_estimators=100)
+model = RandomForestClassifier(n_estimators=params["train"]["n_estimators"])
 model.fit(X, y)
 
 joblib.dump(model, "models/model.pkl")
