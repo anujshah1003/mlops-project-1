36f13885357abb4446853a54e48a4fee1a5a058e
Anuj Shah <20814689+anujshah1003@users.noreply.github.com>
2026-07-01 15:00:11 +0530

    integrating mlflow

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import joblib
 from sklearn.metrics import accuracy_score
+import mlflow
+# read saved run_id
+with open("artifacts/run_id.txt") as f:
+    run_id = f.read().strip()
+    # resume the same run
+mlflow.start_run(run_name=run_id)
 
 df = pd.read_csv("data/processed.csv")
 
@@ -14,3 +20,9 @@ acc = accuracy_score(y, preds)
 
 with open("metrics.txt", "w") as f:
     f.write(f"accuracy: {acc}")
+
+mlflow.log_metric("accuracy", acc)
+mlflow.log_artifact("metrics.txt")
+# mlflow.log_artifact("confusion_matrix.png")
+# mlflow.log_artifact("classification_report.txt")
+mlflow.end_run()
--- src/train2.py
+++ src/train2.py
@@ -2,6 +2,13 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 import yaml
+import mlflow
+
+mlflow.start_run()
+run_id = mlflow.active_run().info.run_id
+print("RUN_ID:", run_id)
+with open("artifacts/run_id.txt", "w") as f:
+    f.write(f"run_id: {run_id}")
 
 params = yaml.safe_load(open("params.yaml"))
 
@@ -14,3 +21,9 @@ model = RandomForestClassifier(n_estimators=params["train"]["n_estimators"])
 model.fit(X, y)
 
 joblib.dump(model, "models/model.pkl")
+
+mlflow.log_param("n_estimators", params["train"]["n_estimators"])
+
+mlflow.sklearn.log_model(model, "model")
+
+mlflow.end_run()
